8b5cf8881054f762c9238a765102e24fbb16eaab
47anjan <anjankarmakar15@gmail.com>
2025-07-06 19:21:19 +0600

    feat: add constants for API base URL and local storage keys

---
+++ utils/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+export const AUTH_TOKEN_KEY = "token";
+export const LOCAL_STORAGE_CART = "carts";
+export const LOCAL_STORAGE_FAVORITE = "favorite";
